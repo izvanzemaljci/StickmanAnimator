0ffe7c94b108a3d651d43fbafcac24d59075cb0b
izvanzemaljci <sanja.milovic@stu.ssst.edu.ba>
2021-05-19 15:31:29 +0200

    made stickman with fixed joints

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private bool isDragged;
+
+    private void OnMouseDown() {
+        isDragged = true;
+    }
+
+    private void OnMouseUp() {
+        isDragged = false;
+    }
+
+    private void Start() {
+        FixedJoint2D joint = gameObject.GetComponent(typeof(FixedJoint2D)) as FixedJoint2D;
+        joint.autoConfigureConnectedAnchor = true;
+        joint.autoConfigureConnectedAnchor = false;
+    }
+
+    void Update()
+    {
+        if(isDragged) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
